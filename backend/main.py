0cb4b4a11df3e496fe5d97f8f3d853180c19b4ec
Ozcan YILDIZHAN <ozcan.14785@gmail.com>
2025-05-11 16:05:19 +0300

    vercel edit

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # CORS ayarları
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://animal-classification-tr.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
